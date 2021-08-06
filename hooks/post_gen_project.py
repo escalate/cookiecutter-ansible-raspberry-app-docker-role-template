39f25abb98d61f617b9f2f43e25941c479e53454
Felix Boerner <ich@felix-boerner.de>
2021-08-06 12:33:27 +0200

    feat: delete funding yaml if url is empty

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,8 +2,12 @@
 
 import os
 
+author_funding_url = "{{ cookiecutter.author_funding_url | trim }}"
 app_config = "{{ cookiecutter.app_config_yaml | lower }}"
 app_backup_job = "{{ cookiecutter.app_backup_job | lower }}"
+
+if len(author_funding_url) == 0:
+    os.remove("./.github/FUNDING.yml")
 
 if app_config == "false":
     os.remove("./templates/{{ cookiecutter.app_name_slug }}.yml.j2")
